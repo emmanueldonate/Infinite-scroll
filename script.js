3126e9b156ad09d63927deacf3a2a6e35cf90c59
Emmanuel <e.o.donate@gmail.com>
2020-07-12 10:41:29 -0700

    Update Photo function

--- script.js
+++ script.js
@@ -23,13 +23,19 @@ function displayPhotos() {
     photosArray.forEach((photo) => {
         // Create <a> to link Unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
-        //Create <img> for photo
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+       
+        // Create <img> for photo
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+       
         //Put <img> inside <a>, then put both inside imageContainer Element
         item.appendChild(img);
         imageContainer.appendChild(item);
